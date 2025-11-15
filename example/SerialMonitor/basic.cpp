c1c58b59883630a6ca3f445ddbf1e19d1a86901d
bamdad <bamdad.kho@gmail.com>
2025-11-15 11:54:13 -0800

    updated serial monitor example

--- example/SerialMonitor/basic.cpp
+++ example/SerialMonitor/basic.cpp
@@ -1,35 +1,20 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdint.h>
-#include <protocol_UART.h>
+#include <Arduino.h>
+#include <device_SerialMonitor.h>
 
-static UART debugUart(&PIND, &DDRD, &PORTD, PD1, &PIND, &DDRD, &PORTD, PD0, 9600UL);
-
-static void debugPrintDecimal(uint32_t value) {
-    char buf[11];
-    uint8_t len = 0;
-    do {
-        buf[len++] = '0' + (value % 10);
-        value /= 10;
-    } while (value != 0);
-    while (len--) {
-        debugUart.sendByte(buf[len]);
-    }
-}
+SerialMonitor Debug;
 
 int main(void) {
-    debugUart.begin();
-    debugUart.sendString("SerialMonitor example start\r\n");
-    debugUart.sendString("Counter: ");
-    debugPrintDecimal(0);
-    debugUart.sendString("\r\n");
+    init();  // Initialize Arduino core
+    Debug.begin(9600);
+    Debug.println("SerialMonitor example start");
+    Debug.print("Counter: ");
+    Debug.println(0);
 
-    uint32_t counter = 0;
+    uint32_t counter = 1;
     while (1) {
-        debugUart.sendString("Counter: ");
-        debugPrintDecimal(counter++);
-        debugUart.sendString("\r\n");
-        _delay_ms(1000);
+        Debug.print("Counter: ");
+        Debug.println(counter++);
+        delay(1000);
     }
 
     return 0;
